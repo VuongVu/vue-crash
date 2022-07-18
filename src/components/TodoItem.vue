b4514483a1836ae6cd0f255ab0e8a5fb803678a0
dan <vu.nguyenvuong@linecorp.com>
2022-07-18 16:52:50 +0700

    Add provide/inject example

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,25 +1,27 @@
 <script setup lang="ts">
-    import { PropType } from 'vue';
+    import { computed, inject, PropType } from 'vue';
     import { CheckIcon, XIcon } from '@heroicons/vue/solid';
     import type { Todo } from '@/stores/useTodoStore';
     import useTodo from '@/composables/useTodo';
 
     const { removeTodo, completeTodo } = useTodo();
 
-    defineProps({
+    const props = defineProps({
         todo: {
             type: Object as PropType<Todo>,
             required: true,
         },
     });
+
+    const hasCompleted = computed(() => props.todo.completed);
+
+    const author = inject('author');
 </script>
 
 <template>
-    <span :class="{ 'line-through italic': todo.completed }">
-        {{ todo.content }}
-    </span>
+    <span :class="{ 'line-through italic': hasCompleted }"> {{ `${todo.content} - ${author}` }} </span>
     <CheckIcon
-        :class="['h-5 text-green-500 hover:cursor-pointer', todo.completed && 'hidden']"
+        :class="['h-5 text-green-500 hover:cursor-pointer', hasCompleted && 'hidden']"
         @click="completeTodo(todo.id)" />
     <XIcon class="h-5 text-red-500 hover:cursor-pointer" @click="removeTodo(todo.id)" />
 </template>
